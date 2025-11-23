e7c1ffb8113449bf4787b8cce4eaa388cce200af
AutoTag User <autotag@example.com>
2025-11-23 16:44:47 +0800

    revert(ui): restore original Tailwind CDN styling and remove StrictMode

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -9,8 +8,4 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
